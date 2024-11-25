ef93fe97e0e1d803331a69305a9b6839235d2d61
Ryan adonis <rr1294989@gmail.com>
2024-11-25 20:03:42 +0100

    e

--- src/allocator.rs
+++ src/allocator.rs
@@ -6,3 +6,9 @@ pub struct SimpleAllocator {
     offset: usize,      
 }
 
+
+pub trait Allocator<T> {
+    type AllocatedMemory : super::AllocatedSlice<T>;
+    fn alloc_cell(&mut self, len : usize) -> Self::AllocatedMemory;
+    fn free_cell(&mut self, data : Self::AllocatedMemory);
+}
