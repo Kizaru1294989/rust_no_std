11ded981b4a1c97ab0614cbca3cc7c03cfe48dc9
ryan <ryan.rais@ynov.com>
2024-11-26 12:34:54 +0100

    memory

--- src/memory.rs
+++ src/memory.rs
@@ -6,4 +6,25 @@ pub struct SlabMemory;
 
 static mut ARENAS: [Option<Arena>; 8] = [None; 8];
 
+impl SlabMemory {
+    pub unsafe fn allocate(layout: Layout) -> *mut u8 {
+        if let Some(block_size) = BlockSize::categorize(layout.size()) {
+            let index = block_size as usize / 8 - 1;
+            if let Some(ref mut arena) = ARENAS[index] {
+                return arena.allocate();
+            }
+        }
+        core::ptr::null_mut()
+    }
 
+    pub unsafe fn deallocate(ptr: *mut u8, layout: Layout) {
+        if let Some(block_size) = BlockSize::categorize(layout.size()) {
+            let index = block_size as usize / 8 - 1;
+            if let Some(ref mut arena) = ARENAS[index] {
+                arena.deallocate(ptr);
+            }
+        }
+    }
+
+
+}
