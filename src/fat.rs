2590c63129c29232cdfad65262b24c9c81dd73d4
Ryan adonis <rr1294989@gmail.com>
2025-02-23 18:41:05 +0100

    fat32

--- src/fat.rs
+++ src/fat.rs
@@ -77,6 +77,86 @@ where
         }
         cluster
     }
+    pub(crate) fn write(&mut self, cluster: u32, value: u32) {
+        let offset = (cluster as usize) * 4;
+        let block_offset = offset / BUFFER_SIZE;
+        let offset_left = offset % BUFFER_SIZE;
+        let offset = self.fat_offset + block_offset * BUFFER_SIZE;
+        let mut value: [u8; 4] = value.to_le_bytes();
 
+        self.device.read(self.buffer, offset, 1).ok();
+        self.buffer[offset_left..offset_left + 4].copy_from_slice(&value);
+        self.device.write(self.buffer, offset, 1).ok();
+    }
+
+    /// Réinitialise le parcours de la FAT
+    pub(crate) fn refresh(&mut self, start_cluster: u32) {
+        self.current_cluster = 0;
+        self.start_cluster = start_cluster;
+    }
+
+    /// Revenir au cluster précédent
+    pub(crate) fn previous(&mut self) {
+        if self.current_cluster != 0 {
+            self.next_cluster = Some(self.current_cluster);
+            self.current_cluster = self.previous_cluster;
+        }
+    }
+
+    /// Vérifie si `next_cluster` est `None`
+    pub(crate) fn next_is_none(&self) -> bool {
+        self.next_cluster.is_none()
+    }
+
+    fn current_cluster_usize(&self) -> usize {
+        self.current_cluster as usize
+    }
+
+    /// Libère la mémoire du buffer lorsque FAT n'est plus utilisé
+    pub(crate) fn free(self) {
+        let layout = Layout::new::<[u8; BUFFER_SIZE]>();
+        unsafe {
+            SlabMemory::deallocate(self.buffer as *mut [u8; BUFFER_SIZE] as *mut u8, layout);
+        }
+    }
+}
+
+impl<T> Iterator for FAT<T>
+where
+    T: BlockDevice + Clone + Copy,
+{
+    type Item = Self;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_cluster == 0 {
+            self.current_cluster = self.start_cluster;
+        } else {
+            let next_cluster = self.next_cluster;
+            if let Some(next_cluster) = next_cluster {
+                self.previous_cluster = self.current_cluster;
+                self.current_cluster = next_cluster;
+            } else {
+                return None;
+            }
+        }
+
+        let offset = self.current_cluster_usize() * 4;
+        let block_offset = offset / BUFFER_SIZE;
+        let offset_left = offset % BUFFER_SIZE;
+
+        self.device.read(self.buffer, self.fat_offset + block_offset * BUFFER_SIZE, 1).ok();
+
+        let next_cluster = read_le_u32(&self.buffer[offset_left..offset_left + 4]);
+        self.next_cluster = if next_cluster == 0x0FFFFFFF {
+            None
+        } else {
+            Some(next_cluster)
+        };
+
+        Some(Self {
+            next_cluster: self.next_cluster,
+            ..(*self)
+        })
+    }
     
 }
